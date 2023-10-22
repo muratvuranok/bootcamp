ea8f21655879175b580e2bf9b3727a39e0042211
muratvuranok <muratvuranok@hotmail.com>
2023-10-22 12:01:01 +0400

    connection string addded

--- FirstWebApi/Data/ApplicationDbContext.cs
+++ FirstWebApi/Data/ApplicationDbContext.cs
@@ -1,10 +1,10 @@
+using BootCamp.FirstWebApi.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace BootCamp.FirstWebApi.Data;
 public class ApplicationDbContext : DbContext  // IdentityDbContext<IdentityUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    public DbSet<Category> Categories { get; set; }
 
-
-    
 }
